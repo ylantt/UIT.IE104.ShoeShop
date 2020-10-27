723aec1ac6fc0b8a1bd4a42bfe9488ec2ac8801a
Lan <truongylan2000@gmail.com>
2020-10-27 17:05:43 +0700

    8. homepage - service section

--- application/views/home.php
+++ application/views/home.php
@@ -3,6 +3,70 @@ require_once("template/header.php");
 require_once("template/nav.php");
 require_once("template/banner.php"); ?>
 
-<div class="home"></div>
+<div class="home">
+     <div class="container">
+          <div class="service">
+               <div class="service__item">
+                    <div class="service__icon">
+                         <i class="fa fa-shipping-fast"></i>
+                    </div>
+
+                    <div class="service__info">
+                         <h4 class="service__heading heading-h4">
+                              FREE SHIPPING
+                         </h4>
+                         <p class="service__content">
+                              Free shipping on all Vietnam order
+                         </p>
+                    </div>
+               </div>
+
+               <div class="service__item">
+                    <div class="service__icon">
+                         <i class="fas fa-phone-volume"></i>
+                    </div>
+
+                    <div class="service__info">
+                         <h4 class="service__heading heading-h4">
+                              ONLINE SUPPORT 24/7
+                         </h4>
+                         <p class="service__content">
+                              Support online 24 hours a day
+                         </p>
+                    </div>
+               </div>
+
+               <div class="service__item">
+                    <div class="service__icon">
+                         <i class="far fa-money-bill-alt"></i>
+                    </div>
+
+                    <div class="service__info">
+                         <h4 class="service__heading heading-h4">
+                              MONEY RETURN
+                         </h4>
+                         <p class="service__content">
+                              Back guarantee under 7 days
+                         </p>
+                    </div>
+               </div>
+
+               <div class="service__item">
+                    <div class="service__icon">
+                         <i class="far fa-id-card"></i>
+                    </div>
+
+                    <div class="service__info">
+                         <h4 class="service__heading heading-h4">
+                              MEMBER DISCOUNT
+                         </h4>
+                         <p class="service__content">
+                              Onevery order over $120.00
+                         </p>
+                    </div>
+               </div>
+          </div>
+     </div>
+</div>
 <?php
 require_once("template/footer.php");
