62bc1696093d41ee3b4ab40086bb7d0f3126bf40
Lan <truongylan2000@gmail.com>
2020-10-30 12:48:43 +0700

    8. homepage - featured project

--- application/views/home.php
+++ application/views/home.php
@@ -5,6 +5,173 @@ require_once("template/banner.php"); ?>
 
 <div class="home">
      <div class="container">
+          <div class="featured-products">
+               <h2 class="heading-h3">
+                    <p class="heading-h3--sub">FEATURED SHOP ITEMS</p>
+                    <p class="heading-h3--main">FEATURED PRODUCTS</p>
+               </h2>
+
+               <div class="product">
+                    <div class="row d-flex">
+                         <div href="#" class="product__item">
+                              <span class="product__new">NEW</span>
+                              <a class="product__image">
+                                   <img src="./public/images/image-1.png" alt="">
+                              </a>
+                              <div class="product__summary">
+                                   <div class="product__info">
+                                        <p class="product__name">Knit Throw</p>
+                                        <p class="product__price">&dollar;35.00</p>
+                                   </div>
+                                   <div class="product__action d-flex">
+                                        <span class="product__add">ADD TO CARD</span>
+                                        <div class="product__action--right">
+                                             <span class="product__view"><i class="far fa-eye product__icon"></i></span>
+                                             <span class="product__love "><i class="far fa-heart product__icon"></i></span>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div href="#" class="product__item">
+                              <a class="product__image">
+                                   <img src="./public/images/image-1.png" alt="">
+                              </a>
+                              <div class="product__summary">
+                                   <div class="product__info">
+                                        <p class="product__name">Knit Throw</p>
+                                        <p class="product__price">&dollar;35.00</p>
+                                   </div>
+                                   <div class="product__action d-flex">
+                                        <span class="product__add">ADD TO CARD</span>
+                                        <div class="product__action--right">
+                                             <span class="product__view"><i class="far fa-eye product__icon"></i></span>
+                                             <span class="product__love "><i class="far fa-heart product__icon"></i></span>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div href="#" class="product__item">
+                              <a class="product__image">
+                                   <img src="./public/images/image-1.png" alt="">
+                              </a>
+                              <div class="product__summary">
+                                   <div class="product__info">
+                                        <p class="product__name">Knit Throw</p>
+                                        <p class="product__price">&dollar;35.00</p>
+                                   </div>
+                                   <div class="product__action d-flex">
+                                        <span class="product__add">ADD TO CARD</span>
+                                        <div class="product__action--right">
+                                             <span class="product__view"><i class="far fa-eye product__icon"></i></span>
+                                             <span class="product__love "><i class="far fa-heart product__icon"></i></span>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div href="#" class="product__item">
+                              <a class="product__image">
+                                   <img src="./public/images/image-1.png" alt="">
+                              </a>
+                              <div class="product__summary">
+                                   <div class="product__info">
+                                        <p class="product__name">Knit Throw</p>
+                                        <p class="product__price">&dollar;35.00</p>
+                                   </div>
+                                   <div class="product__action d-flex">
+                                        <span class="product__add">ADD TO CARD</span>
+                                        <div class="product__action--right">
+                                             <span class="product__view"><i class="far fa-eye product__icon"></i></span>
+                                             <span class="product__love "><i class="far fa-heart product__icon"></i></span>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="row d-flex">
+                         <div href="#" class="product__item">
+                              <a class="product__image">
+                                   <img src="./public/images/image-1.png" alt="">
+                              </a>
+                              <div class="product__summary">
+                                   <div class="product__info">
+                                        <p class="product__name">Knit Throw</p>
+                                        <p class="product__price">&dollar;35.00</p>
+                                   </div>
+                                   <div class="product__action d-flex">
+                                        <span class="product__add">ADD TO CARD</span>
+                                        <div class="product__action--right">
+                                             <span class="product__view"><i class="far fa-eye product__icon"></i></span>
+                                             <span class="product__love "><i class="far fa-heart product__icon"></i></span>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div href="#" class="product__item">
+                              <a class="product__image">
+                                   <img src="./public/images/image-1.png" alt="">
+                              </a>
+                              <div class="product__summary">
+                                   <div class="product__info">
+                                        <p class="product__name">Knit Throw</p>
+                                        <p class="product__price">&dollar;35.00</p>
+                                   </div>
+                                   <div class="product__action d-flex">
+                                        <span class="product__add">ADD TO CARD</span>
+                                        <div class="product__action--right">
+                                             <span class="product__view"><i class="far fa-eye product__icon"></i></span>
+                                             <span class="product__love "><i class="far fa-heart product__icon"></i></span>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div href="#" class="product__item">
+                              <span class="product__sale">SALE</span>
+                              <a class="product__image">
+                                   <img src="./public/images/image-1.png" alt="">
+                              </a>
+                              <div class="product__summary">
+                                   <div class="product__info">
+                                        <p class="product__name">Knit Throw</p>
+                                        <p class="product__price">&dollar;35.00</p>
+                                   </div>
+                                   <div class="product__action d-flex">
+                                        <span class="product__add">ADD TO CARD</span>
+                                        <div class="product__action--right">
+                                             <span class="product__view"><i class="far fa-eye product__icon"></i></span>
+                                             <span class="product__love "><i class="far fa-heart product__icon"></i></span>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div href="#" class="product__item">
+                              <a class="product__image">
+                                   <img src="./public/images/image-1.png" alt="">
+                              </a>
+                              <div class="product__summary">
+                                   <div class="product__info">
+                                        <p class="product__name">Knit Throw</p>
+                                        <p class="product__price">&dollar;35.00</p>
+                                   </div>
+                                   <div class="product__action d-flex">
+                                        <span class="product__add">ADD TO CARD</span>
+                                        <div class="product__action--right">
+                                             <span class="product__view"><i class="far fa-eye product__icon"></i></span>
+                                             <span class="product__love "><i class="far fa-heart product__icon"></i></span>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div>
+
           <div class="service">
                <div class="service__item">
                     <div class="service__icon">
