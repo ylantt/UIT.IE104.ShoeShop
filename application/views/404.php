17e227079093538b6aea67f47fcc66840ce145ae
18520566 <73072113+18520566@users.noreply.github.com>
2020-10-31 14:08:56 +0700

    1.404 3.detail

--- application/views/404.php
+++ application/views/404.php
@@ -1,6 +1,30 @@
 <?php
 include "template/header.php";
 include "template/nav.php"; ?>
-<!-- code ở chỗgiữa ni nhaaaaaa -->
+    <div >
+        <div class=".ErrorHeader" >
+            <div style="margin:10 px 0px 10px 0px; padding: 290px 0px 10px 200px">
+                <p  class="ErrorTitle">
+                    ERROR 404</p>
+                <p class="ErrorDetail" >Oops.</p>
+                    <p class="ErrorDetails">
+                        The page you're looking for isn't available. <br>
+                        Try to search again or use to go back button below.
+                    </p>
+                    <p>
+                        <a href="ErrorPage.html" class="button large">
+                            <span class= ".svg-icon icon-arrow-left size normal">
+                            
+                                </use>
+                            </svg>
+                        </span>
+                            Go back
+                        </a>
+                        </p>
+            </div>
+            
+        </div>
+
+</div>
 <?php
 include "template/footer.php";
--- application/views/detail.php
+++ application/views/detail.php
@@ -1,6 +1,78 @@
 <?php
 include "template/header.php";
 include "template/nav.php"; ?>
-<!-- code ở chỗgiữa ni nhaaaaaa -->
+<div>
+<div class="container_bg">
+<div class="container">
+    <nav aria-label="breadcrumb">
+        <ul class="breadcrumb">
+            <li class="breadcrumb-items">
+                <a href="ErrorPage.html" style="color:whitesmoke;text-decoration: none;">Home</a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">
+                 Product Details
+            </li>
+        </ul>
+    </nav>
+</div>
+</div>
+<div class="container">
+    <div class="row">
+        <div>
+            <img src="public/images/tui-lv-hop-6.jpg" >
+        </div>
+        <div class="productdetails">
+            <h1>LV Bag </h1>
+            <span class="price">$1200.00
+            <span class="stock">
+                <b>In stock</b>  
+                <span>
+                    <span>SKU </span>
+                    24-MB01
+                </span>
+            </span>
+            <div class="qty-add-cart">
+                <label for="qty">Quantity</label>
+                <input type="number" placeholder="1" id="qty">
+                <a href=cai_gio_hang.html>Add to cart</a>
+            </div>
+            <div class="buttons">
+                <a href="#">Wish list</a>
+                <a href="#">Compare</a>
+            </div>
+            <p>This is a productive product create by Luon Vuituoi<br>
+            This product is suitable for anyone as long as they have money<br>
+            This not too expendsive but only 1000$
+            </p>
+        </span>
+        </div>
+    </div>
+<div class="details_area">
+    <div class="details_container">
+        <div class="details_buttons">
+            <div class="nav" role="tablist">
+                <a class="active" href="#tab1" data-toggle="tab" aria-selected="true" aria-controls="tab1">Product Details</a>
+            </div>
+        </div>
+        <div class=".details_container">
+            <div class="details_items">
+                <div class="details_items_container">
+                    <p>Convenience is next to nothing when your day is crammed with action. So whether you're heading to class, gym, or the unbeaten path, make sure you've got your Strive Shoulder Pack stuffed with all your essentials, and extras as well.                        
+                    </p>
+                    <ul>
+                    <li>
+                        Zippered main compartment.</li>
+                    <li>Front zippered pocket.</li>
+                    <li>Waterproof chair</li>
+                    <li>CManual functional</li>
+                    <li>Atheletics Design</li>
+                </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
 <?php
 include "template/footer.php";
