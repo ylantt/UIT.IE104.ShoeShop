56b872ef3141796f0a127f4310f7d05995a2e738
18520566 <73072113+18520566@users.noreply.github.com>
2020-11-02 16:20:55 +0700

    1. 404 - update 1

--- application/views/404.php
+++ application/views/404.php
@@ -2,18 +2,18 @@
 include "template/header.php";
 include "template/nav.php"; ?>
     <div >
-        <div class=".ErrorHeader" >
-            <div style="margin:10 px 0px 10px 0px; padding: 290px 0px 10px 200px">
-                <p  class="ErrorTitle">
+        <div class=".error__errorHeader" >
+            <div class="error__body">
+                <p  class="error__errortitle">
                     ERROR 404</p>
-                <p class="ErrorDetail" >Oops.</p>
-                    <p class="ErrorDetails">
+                <p class="error__errordetail" >Oops.</p>
+                    <p class="error__errordetails">
                         The page you're looking for isn't available. <br>
                         Try to search again or use to go back button below.
                     </p>
                     <p>
-                        <a href="ErrorPage.html" class="button large">
-                            <span class= ".svg-icon icon-arrow-left size normal">
+                        <a href="home" class="error__button large">
+                            <span class= "error__but-icon icon-arrow-left size normal">
                             
                                 </use>
                             </svg>
