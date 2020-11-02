c36136c5015246088ad2b0906f21843f9bb54734
Phan Hoàng Dung <18520027@gm.uit.edu.vn>
2020-11-02 23:23:13 +0700

    2. Cart - Update 1

--- application/views/cart.php
+++ application/views/cart.php
@@ -3,48 +3,52 @@ include "template/header.php";
 include "template/nav.php"; ?>
 
 <div>
-    <h3 class="cart__title">Cart</h3>
-    <div class = "cart__page">
-        <table class="cart__table">
-            <tr>
-                <th class="cart__header">Product</th>
-                <th class="cart__header">Quantity</th>
-                <th class="cart__header">Subtotal</th>
-            </tr>
+    <div class ="container">
+        <h3 class="cart__title">Cart</h3>
+        <div class = "cart__page">
+            <table class="cart__table">
+                <tr>
+                    <th class="cart__header">Product</th>
+                    <th class="cart__header">Quantity</th>
+                    <th class="cart__header">Subtotal</th>
+                </tr>
 
-            <tr>
-                <td>
-                    <div class="cart__info">
-                        <img src="public/images/UtraBoost_Summer.jpg" alt="Ultra boost">
-                        <div>
-                            <p>ULTRABOOST SUMMER.RDY - White </p>
-                            <small>Price: $216,25</small>
-                            </br>
-                            <a href="">Remove</a>
+                <tr>
+                    <td>
+                        <div class="cart__info">
+                            <img src="public/images/UtraBoost_Summer.jpg" alt="Ultra boost">
+                            <div>
+                                <p>ULTRABOOST SUMMER.RDY - White </p>
+                                <small>Price: $216,25</small>
+                                </br>
+                                <a href="">Remove</a>
+                            </div>
                         </div>
-                    </div>
-                </td>
-                <td><input type="number" value="1" min="1" max="999"/></td>
-                <td>$216,25</td>
-            </tr>
+                    </td>
+                    <td><input type="number" value="1" min="1" max="999"/></td>
+                    <td>$216,25</td>
+                </tr>
 
-            <tr>
-            <td>
-                    <div class="cart__info">
-                        <img src="public/images/UtraBoost_Summer.jpg" alt="Ultra boost">
-                        <div>
-                            <p>ULTRABOOST SUMMER.RDY </p>
-                            <small>Price: $220,00</small>
-                            </br>
-                            <a href="">Remove</a>
+                <tr>
+                    <td>
+                        <div class="cart__info">
+                            <img src="public/images/UtraBoost_Summer.jpg" alt="Ultra boost">
+                            <div>
+                                <p>ULTRABOOST SUMMER.RDY </p>
+                                <small>Price: $220,00</small>
+                                </br>
+                                <a href="">Remove</a>
+                            </div>
                         </div>
-                    </div>
-                </td>
-                <td><input type="number" value="1" min="1" max="999"/></td>
-                <td>$220,00</td>
-            </tr>
-        </table>
+                    </td>
+                    <td><input type="number" value="1" min="1" max="999"/></td>
+                    <td>$220,00</td>
+                </tr>
+            </table>
+        </div>
+    </div>
 
+    <div class ="container">
         <div class= "cart__total">
             <table>
                 <tr>
@@ -62,8 +66,10 @@ include "template/nav.php"; ?>
             </table>
         </div>
 
-        <input class="cart__OrdBtn" type="button" name="" value="Proceed to checkout"/>
+            <input class="cart__OrdBtn" type="button" name="" value="Proceed to checkout"/>
     </div>
+    
+
 </div>
 
 <?php
