dfb600a26ebdd86b439399075060032206f2d98c
18520566 <73072113+18520566@users.noreply.github.com>
2020-11-02 16:56:26 +0700

    3. detail - update 1

--- application/views/detail.php
+++ application/views/detail.php
@@ -2,61 +2,60 @@
 include "template/header.php";
 include "template/nav.php"; ?>
 <div>
-<div class="container_bg">
-<div class="container">
-    <nav aria-label="breadcrumb">
-        <ul class="breadcrumb">
-            <li class="breadcrumb-items">
-                <a href="ErrorPage.html" style="color:whitesmoke;text-decoration: none;">Home</a>
+<div class="detail__container_bg">
+<div class="detail__container">
+    <nav aria-label="detail__breadcrumb">
+        <ul class="detail__breadcrumb">
+            <li class="detail__breadcrumb-items">
+                <a href="home" style="color:whitesmoke;text-decoration: none;">Home</a>
             </li>
-            <li class="breadcrumb-item active" aria-current="page">
+            <li class="detail__breadcrumb-item active" aria-current="page">
                  Product Details
             </li>
         </ul>
     </nav>
 </div>
 </div>
-<div class="container">
-    <div class="row">
+<div class="detail__container">
+    <div class="detail__row">
         <div>
-            <img src="public/images/tui-lv-hop-6.jpg" >
+            <img src="public/images/giay-nike-chinh-hang.jpg" >
         </div>
-        <div class="productdetails">
-            <h1>LV Bag </h1>
-            <span class="price">$1200.00
-            <span class="stock">
+        <div class="detail__productdetails">
+            <h1>Nike Shoes </h1>
+            <span class="detail__price">$1200.00
+            <span class="detail__stock">
                 <b>In stock</b>  
                 <span>
                     <span>SKU </span>
                     24-MB01
                 </span>
             </span>
-            <div class="qty-add-cart">
-                <label for="qty">Quantity</label>
-                <input type="number" placeholder="1" id="qty">
-                <a href=cai_gio_hang.html>Add to cart</a>
+            <div class="detail__qty-add-cart">
+                <label for="detail__qty">Quantity</label>
+                <input type="number" placeholder="1" id="detail__qty">
+                <a href=cart>Add to cart</a>
             </div>
-            <div class="buttons">
-                <a href="#">Wish list</a>
-                <a href="#">Compare</a>
+            <div class="detail__buttons">
+                <a href="wishlist">Wish list</a>
             </div>
-            <p>This is a productive product create by Luon Vuituoi<br>
+            <p class="detail__final">This is a productive product create by Luon Vuituoi<br>
             This product is suitable for anyone as long as they have money<br>
             This not too expendsive but only 1000$
             </p>
         </span>
         </div>
     </div>
-<div class="details_area">
-    <div class="details_container">
-        <div class="details_buttons">
+<div class="detail__area">
+    <div class="detail__container">
+        <div class="detail__buttons">
             <div class="nav" role="tablist">
-                <a class="active" href="#tab1" data-toggle="tab" aria-selected="true" aria-controls="tab1">Product Details</a>
+                <a style="color:black;padding-left:5rem">Product Details</a>
             </div>
         </div>
-        <div class=".details_container">
-            <div class="details_items">
-                <div class="details_items_container">
+        <div class=".detail__container">
+            <div class="detail__items">
+                <div class="detail__items_container">
                     <p>Convenience is next to nothing when your day is crammed with action. So whether you're heading to class, gym, or the unbeaten path, make sure you've got your Strive Shoulder Pack stuffed with all your essentials, and extras as well.                        
                     </p>
                     <ul>
