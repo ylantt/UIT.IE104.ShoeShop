05f099002bbfa357e1cd7b0e35d15c6cb02c0cb0
Lan <truongylan2000@gmail.com>
2020-11-03 13:08:00 +0700

    1. 404 update 2 (Lan update)

--- application/views/404.php
+++ application/views/404.php
@@ -1,30 +1,22 @@
 <?php
 include "template/header.php";
 include "template/nav.php"; ?>
-    <div >
-        <div class=".error__errorHeader" >
-            <div class="error__body">
-                <p  class="error__errortitle">
-                    ERROR 404</p>
-                <p class="error__errordetail" >Oops.</p>
-                    <p class="error__errordetails">
-                        The page you're looking for isn't available. <br>
-                        Try to search again or use to go back button below.
-                    </p>
-                    <p>
-                        <a href="home" class="error__button large">
-                            <span class= "error__but-icon icon-arrow-left size normal">
-                            
-                                </use>
-                            </svg>
-                        </span>
-                            Go back
-                        </a>
-                        </p>
-            </div>
-            
+<div class="container">
+    <div class=".error">
+        <div class="error__body">
+            <p class="error__title">ERROR 404</p>
+            <p class="error__detail">Oops.</p>
+            <p class="error__more-detail">
+                The page you're looking for isn't available. <br>
+                Try to search again or use to go back button below.
+            </p>
+            <p>
+                <a href="home" class="error__button error__button--large">
+                    &LeftArrow; Go back
+                </a>
+            </p>
         </div>
-
+    </div>
 </div>
 <?php
 include "template/footer.php";
