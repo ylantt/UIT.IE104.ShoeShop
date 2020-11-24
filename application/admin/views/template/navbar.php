4c893b46d7a101059a9ad082d433f7cadb60ffbd
Lan <truongylan2000@gmail.com>
2020-11-24 22:02:51 +0700

    restructure websites

---
+++ application/admin/views/template/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+     <div class="collapse navbar-collapse col-10 ml-auto p-0" id="navbarSupportedContent">
+          <p class="navbar-nav mr-auto">
+               <a class="nav-link" href="http://ie104project.test/home">Home <span class="sr-only">(current)</span></a>
+          </p>
+          
+          <form class="form-inline my-2 my-lg-0">
+               <input class="form-control mr-sm-2" type="search"      placeholder="Search" aria-label="Search">
+               <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+          </form>
+
+          <p class="navbar-nav ml-3">
+               <a class="nav-link" href="#">admin</a>
+          </p>
+     </div>
+</nav>
