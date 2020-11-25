807f9f54f9475364ad8715ad76b80980ba11991b
Lan <truongylan2000@gmail.com>
2020-11-25 15:02:31 +0700

    update navbar admin page

--- application/admin/views/template/navbar.php
+++ application/admin/views/template/navbar.php
@@ -4,11 +4,6 @@
                <a class="nav-link" href="http://ie104project.test/home">Home <span class="sr-only">(current)</span></a>
           </p>
           
-          <form class="form-inline my-2 my-lg-0">
-               <input class="form-control mr-sm-2" type="search"      placeholder="Search" aria-label="Search">
-               <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-          </form>
-
           <p class="navbar-nav ml-3">
                <a class="nav-link" href="#">admin</a>
           </p>
