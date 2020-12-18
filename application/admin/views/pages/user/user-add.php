a3b18b6a37db2f8b38d7d0ad900c8c7f011f5c1c
18520566 <73072113+18520566@users.noreply.github.com>
2020-12-18 12:50:20 +0700

    trang add user - update 1

---
+++ application/admin/views/pages/user/user-add.php
@@ -0,0 +1,74 @@
+<div class="d-flex justify-content-between  pt-3">
+  <h1 class="text-primary text-uppercase">
+    Add User ID 001
+  </h1>
+</div>
+<hr>
+<div class="ml-2">
+  <form>
+    <div class="form-group row">
+      <div class="col-sm-2">
+        <label>Image</label>
+      </div>
+      <div class="col-sm-1">
+        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+      </div>
+    </div>
+    <div class="form-group row">
+      <div class="col-sm-2">
+        <label>Username</label>
+      </div>
+      <div class="col-sm-5">
+        <textarea class="form-control" id="desc" rows="1"></textarea>
+      </div>
+    </div>
+    <div class="form-group row">
+      <div class="col-sm-2">
+        <label>Email:</label>
+      </div>
+      <div class="col-sm-5">
+        <textarea class="form-control" id="shortdesc" rows="1" maxlength="250"></textarea>
+      </div>
+    </div>
+    <div class="form-group row">
+      <div class="col-sm-2">
+        <label>Full name:</label>
+      </div>
+      <div class="col-sm-5">
+      <textarea class="form-control" id="shortdesc" rows="1" maxlength="250"></textarea>
+      </div>
+    </div>
+    <div class="form-group row">
+      <div class="col-sm-2">
+        <label>Address:</label>
+      </div>
+      <div class="col-sm-5">
+      <textarea class="form-control" id="shortdesc" rows="3" maxlength="250"></textarea>
+      </div>
+    </div>
+    <div class="form-group row">
+      <div class="col-sm-2">
+        <label>Phone:</label>
+      </div>
+      <div class="col-sm-5">
+      <textarea class="form-control" id="shortdesc" rows="1" maxlength="250"></textarea>
+      </div>
+    </div>
+    <div class="form-group row">
+      <div class="col-sm-2">
+        <label>Role:</label>
+      </div>
+      <div class="col-sm-2">
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+          <label class="form-check-label" for="inlineRadio1">User</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+          <label class="form-check-label" for="inlineRadio2">Admin</label>
+        </div>
+      </div>
+    </div>
+    <button class="btn btn-success pt-2 md-2">Add</button>
+  </form>
+</div>
