894acc9c0415c743068d7f09aa8218d3e8db1d1c
Lan <truongylan2000@gmail.com>
2020-12-20 00:03:14 +0700

    home product section backend handle

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,8 +1,24 @@
 <?php
 class Home extends Controller
 {
+    function __construct()
+    {
+        $this->cate = $this->model("catedb");
+        $this->product = $this->model("productdb");
+    }
+
     public function show()
     {
-        $this->view("home");
+        $rowProduct = $this->product->getProduct(8);
+        $rowCate = $this->cate->getCate();
+        $cateNameList = array();
+        foreach ($rowCate as $row) {
+            $cateNameList[$row["CategoryID"]] = $row["CategoryName"];
+        }
+
+        $this->view("home", [
+            "info" => $rowProduct,
+            "cate" => $cateNameList
+        ]);
     }
 }
