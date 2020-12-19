84be612c2b70465f4b8838e10b6b843ddb615585
Lan <truongylan2000@gmail.com>
2020-12-19 09:35:12 +0700

    dashboard update UI

--- application/admin/views/pages/dashboard.php
+++ application/admin/views/pages/dashboard.php
@@ -1,51 +1,67 @@
-<div class="row">
-     <div class="col-sm-4">
-          <div class="card text-white bg-danger mb-3 mt-5" style="max-width: 18rem;">
-               <div class="card-header font-weight-bold" style="font-size: 20px;">SALES</div>
-               <div class="card-body">
-                    <h5 class="card-title font-weight-normal" style="font-size: 16px;">Total sales in this year</h5>
-                    <p class="card-text text-center font-weight-bold" style="font-size: 40px;">10<span>&dollar;</span></p>
-               </div>
+<h1 class="text-center text-danger font-weight-bold">SHOP N7</h1>
+<div class="d-flex justify-content-between mt-5">
+     <div class="card">
+          <div class="card-body">
+               <h5 class="card-title">SALES</h5>
+               <h6 class="card-subtitle mb-2 text-muted">Total sales in this year</h6>
+               <p class="card-text font-weight-bold" style="font-size: 50px">1234$</p>
           </div>
      </div>
 
-     <div class="col-sm-4">
-          <div class="card text-white bg-primary mb-3 mt-5" style="max-width: 18rem;">
-               <div class="card-header font-weight-bold" style="font-size: 20px;">Orders</div>
-               <div class="card-body">
-                    <h5 class="card-title font-weight-normal" style="font-size: 16px;">The number of delivered orders</h5>
-                    <p class="card-text text-center font-weight-bold" style="font-size: 40px;">10</p>
-               </div>
+     <div class="card">
+          <div class="card-body">
+               <h5 class="card-title">Orders</h5>
+               <h6 class="card-subtitle mb-2 text-muted">TThe number of delivered orders of this month</h6>
+               <p class="card-text font-weight-bold" style="font-size: 50px">120</p>
           </div>
      </div>
 
-     <div class="col-sm-4">
-          <div class="card text-white bg-success mb-3 mt-5" style="max-width: 18rem;">
-               <div class="card-header font-weight-bold" style="font-size: 20px;">Categories</div>
-               <div class="card-body">
-                    <h5 class="card-title font-weight-normal" style="font-size: 16px;">The number of categories</h5>
-                    <p class="card-text text-center font-weight-bold" style="font-size: 40px;">10</p>
-               </div>
-          </div>
-     </div>
-
-     <div class="col-sm-4">
-          <div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
-               <div class="card-header font-weight-bold" style="font-size: 20px;">Products</div>
-               <div class="card-body">
-                    <h5 class="card-title font-weight-normal" style="font-size: 16px;">The number of available products</h5>
-                    <p class="card-text text-center font-weight-bold" style="font-size: 40px;">10</p>
-               </div>
-          </div>
-     </div>
-
-     <div class="col-sm-4">
-          <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-               <div class="card-header font-weight-bold" style="font-size: 20px;">Users</div>
-               <div class="card-body">
-                    <h5 class="card-title font-weight-normal" style="font-size: 16px;">The number of users in system</h5>
-                    <p class="card-text text-center font-weight-bold" style="font-size: 40px;">10</p>
-               </div>
+     <div class="card">
+          <div class="card-body">
+               <h5 class="card-title">Users</h5>
+               <h6 class="card-subtitle mb-2 text-muted">The number of users in system</h6>
+               <p class="card-text font-weight-bold" style="font-size: 50px">100</p>
           </div>
      </div>
 </div>
+
+<div id="linechart_material" style="width: 100%; height: 450px;" class="ml-auto mr-auto mt-5 mb-3"></div>
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+     google.charts.load('current', {'packages':['line']});
+      google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'Day');
+      data.addColumn('number', 'Sales');
+
+      data.addRows([
+        [1, 80.9],
+        [2, 69.5],
+        [3, 57],
+        [4, 18.8],
+        [5, 17.6],
+        [6, 13.6],
+        [7, 12.3],
+        [8, 29.2],
+        [9, 42.9],
+        [10, 30.9],
+      ]);
+
+      var options = {
+        chart: {
+          title: 'Sales of the last 10 days',
+          subtitle: 'in dollars (USD)'
+        },
+        width: 1070,
+        height: 500
+      };
+
+      var chart = new google.charts.Line(document.getElementById('linechart_material'));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    }
+</script>
