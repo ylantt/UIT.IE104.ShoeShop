6633dd0a6cac8610c3d101cbde4a8299b03ea785
18520566 <73072113+18520566@users.noreply.github.com>
2020-12-19 20:41:12 +0700

    12 - trang about - update 2

--- application/views/about.php
+++ application/views/about.php
@@ -14,10 +14,56 @@ include "template/nav.php"; ?>
                     </li>
                 </ul>
             </nav>
+<div class="about__about-team">
+    <div class="about__about-team-title">
+        About Us
+    </div>
+    <hr>
+    <div class="about__about-team-para">
+    Our mission is what drives us to do everything possible to expand human potential.
+    We do that by creating groundbreaking sport innovations, by making our products more sustainably,
+    by building a creative and diverse global team and by making a positive impact in communities where we live and work.
+    </div>
+</div>
+<div class="about__about-smallteam">
+    <div class="about__about-team-para">
+       <div> <img src="public/images/about1.jpg" ></div>
+        <div>
+        <div class="about__about-team-smalltitle">
+        Innovation
+    </div>
+            <div class="about__about-team-smallertitle">
+            We dare to sell the best
+        </div>
+       <p> To makes big leap, we choose to do big. 
+    </p>
+    </div>
+    </div>
+</div>
+<div class="about__about-smallteam">
+    <div class="about__about-team-para">
+        <div>
+        <div class="about__about-team-smalltitle">
+        Our team
+    </div>
+            <div class="about__about-team-smallertitle">
+            WE ARE EMPOWERED, DIVERSE AND INCLUSIVE
+        </div>
+       <p> A love of sport unites us. 
+    </p>
+    </div>
+    <div> <img src="public/images/about2.jpg" ></div>
+    </div>
+</div>
+<div class="container">
+<div class="about__about-team-title">
+                    Our team
+                </div>
+
             <div class="about__gallery-container">
             <div class="about__gallery">
-                <a target="_blank" href="public/images/giay-nike-chinh-hang.jpg">
-                <img src="public/images/giay-nike-chinh-hang.jpg" alt="Dat" width="600" height="400">     
+                <a target="_blank" href="public/images/lan.jpg">
+                <img src="public/images/lan.jpg" alt="Dat" width="600" height="400">     
             </a>
             <div class="about__desc">
                 <p>
@@ -26,26 +72,26 @@ include "template/nav.php"; ?>
                 <ul>
                     <li>MSSV</li>
                     <li>SDT</li>
-                    <li>Sở thích</li>
+                    <li>E-mail</li>
                 </ul>
             </div>
             </div>
             <div class="about__gallery">
-                <a target="_blank" href="public/images/giay-nike-chinh-hang.jpg">
-                <img src="public/images/giay-nike-chinh-hang.jpg" alt="Dat" width="600" height="400">     
+                <a target="_blank" href="public/images/dat.jpg">
+                <img src="public/images/dat.jpg" alt="Dat" width="600" height="400">     
             </a>
             <div class="about__desc">
                 <p>Hoàng Tiến Đạt</p>
                 <ul>
-                    <li>18520566</li>
-                    <li>032 Đạt có vợ rùi</li>
-                    <li>Đạt thích viết Rap  </li>
+                    <li>MSSV</li>
+                    <li>SDT</li>
+                    <li>E-mail</li>
                 </ul>
             </div>
             </div>
             <div class="about__gallery">
-                <a target="_blank" href="public/images/giay-nike-chinh-hang.jpg">
-                <img src="public/images/giay-nike-chinh-hang.jpg" alt="Dat" width="600" height="400">     
+                <a target="_blank" href="public/images/han.jpg">
+                <img src="public/images/han.jpg" alt="Dat" width="600" height="400">     
             </a>
             <div class="about__desc">
                 <p>
@@ -54,17 +100,17 @@ include "template/nav.php"; ?>
                 <ul>
                     <li>MSSV</li>
                     <li>SDT</li>
-                    <li>Sở thích</li>
+                    <li>E-mail</li>
                 </ul>
             </div>
             </div>
             <div class="about__gallery">
-                <a target="_blank" href="public/images/giay-nike-chinh-hang.jpg">
-                <img src="public/images/giay-nike-chinh-hang.jpg" alt="Dat" width="600" height="400">     
+                <a target="_blank" href="public/images/dung.jpg">
+                <img src="public/images/dung.jpg" alt="Dat" width="600" height="400">     
             </a>
             <div class="about__desc">
                 <p>
-                    Dung 
+                  Phan Hoàng Dung 
                 </p>
                 <ul>
                     <li>MSSV</li>
@@ -74,6 +120,6 @@ include "template/nav.php"; ?>
             </div>
             </div>
             </div> 
-
+            </div>
 <?php
 include "template/footer.php";
