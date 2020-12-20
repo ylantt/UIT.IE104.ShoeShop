7a527d8dbc959b2f9b62eb8da7c9fc198a6cd3ea
Lan <truongylan2000@gmail.com>
2020-12-20 07:56:41 +0700

    about page update 1

--- application/views/about.php
+++ application/views/about.php
@@ -3,123 +3,124 @@ include "template/header.php";
 include "template/nav.php"; ?>
 
 <div class="container">
-<nav>
-                <ul class="breadcrumb">
-                    <li class="breadcrumb-items">
-                        <a href="home">Home</a>
-                    </li>
-                    <span>&ThickSpace;&#62;&ThickSpace;</span>
-                    <li class="breadcrumb-item active">
-                        About us
-                    </li>
-                </ul>
-            </nav>
-<div class="about__about-team">
-    <div class="about__about-team-title">
-        About Us
-    </div>
-    <hr>
-    <div class="about__about-team-para">
-    Our mission is what drives us to do everything possible to expand human potential.
-    We do that by creating groundbreaking sport innovations, by making our products more sustainably,
-    by building a creative and diverse global team and by making a positive impact in communities where we live and work.
-    </div>
-</div>
-<div class="about__about-smallteam">
-    <div class="about__about-team-para">
-       <div> <img src="public/images/about1.jpg" ></div>
-        <div>
-        <div class="about__about-team-smalltitle">
-        Innovation
-    </div>
-            <div class="about__about-team-smallertitle">
-            We dare to sell the best
+    <nav>
+        <ul class="breadcrumb">
+            <li class="breadcrumb-items">
+                <a href="home">Home</a>
+            </li>
+            <span>&ThickSpace;&#62;&ThickSpace;</span>
+            <li class="breadcrumb-item active">
+                About us
+            </li>
+        </ul>
+    </nav>
+    <div class="about__about-team">
+        <div class="about__about-team-title">
+            About Us
         </div>
-       <p> To makes big leap, we choose to do big. 
-    </p>
-    </div>
-    </div>
-</div>
-<div class="about__about-smallteam">
-    <div class="about__about-team-para">
-        <div>
-        <div class="about__about-team-smalltitle">
-        Our team
-    </div>
-            <div class="about__about-team-smallertitle">
-            WE ARE EMPOWERED, DIVERSE AND INCLUSIVE
+        <hr>
+        <div class="about__about-team-para">
+            Our mission is what drives us to do everything possible to expand human potential.
+            We do that by creating groundbreaking sport innovations, by making our products more sustainably,
+            by building a creative and diverse global team and by making a positive impact in communities where we live and work.
         </div>
-       <p> A love of sport unites us. 
-    </p>
     </div>
-    <div> <img src="public/images/about2.jpg" ></div>
+    <div class="about__about-smallteam">
+        <div class="about__about-team-para">
+            <div> <img src="public/images/about1.jpg"></div>
+            <div>
+                <div class="about__about-team-smalltitle">
+                    Innovation
+                </div>
+                <div class="about__about-team-smallertitle">
+                    We dare to sell the best
+                </div>
+                <p> To makes big leap, we choose to do big.
+                </p>
+            </div>
+        </div>
     </div>
-</div>
-<div class="container">
-<div class="about__about-team-title">
+    <div class="about__about-smallteam">
+        <div class="about__about-team-para">
+            <div>
+                <div class="about__about-team-smalltitle">
                     Our team
                 </div>
+                <div class="about__about-team-smallertitle">
+                    WE ARE EMPOWERED, DIVERSE AND INCLUSIVE
+                </div>
+                <p> A love of sport unites us.
+                </p>
+            </div>
+            <div> <img src="public/images/about2.jpg"></div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="about__about-team-title">
+            Our team
+        </div>
 
-            <div class="about__gallery-container">
+        <div class="about__gallery-container">
             <div class="about__gallery">
                 <a target="_blank" href="public/images/lan.jpg">
-                <img src="public/images/lan.jpg" alt="Dat" width="600" height="400">     
-            </a>
-            <div class="about__desc">
-                <p>
-                    Trương Ý Lan
-                </p>
-                <ul>
-                    <li>MSSV</li>
-                    <li>SDT</li>
-                    <li>E-mail</li>
-                </ul>
-            </div>
+                    <img src="public/images/lan.jpg" alt="Dat" width="600" height="400">
+                </a>
+                <div class="about__desc">
+                    <p>
+                        Trương Ý Lan
+                    </p>
+                    <ul>
+                        <li>MSSV</li>
+                        <li>SDT</li>
+                        <li>E-mail</li>
+                    </ul>
+                </div>
             </div>
             <div class="about__gallery">
                 <a target="_blank" href="public/images/dat.jpg">
-                <img src="public/images/dat.jpg" alt="Dat" width="600" height="400">     
-            </a>
-            <div class="about__desc">
-                <p>Hoàng Tiến Đạt</p>
-                <ul>
-                    <li>MSSV</li>
-                    <li>SDT</li>
-                    <li>E-mail</li>
-                </ul>
-            </div>
+                    <img src="public/images/dat.jpg" alt="Dat" width="600" height="400">
+                </a>
+                <div class="about__desc">
+                    <p>Hoàng Tiến Đạt</p>
+                    <ul>
+                        <li>MSSV</li>
+                        <li>SDT</li>
+                        <li>E-mail</li>
+                    </ul>
+                </div>
             </div>
             <div class="about__gallery">
                 <a target="_blank" href="public/images/han.jpg">
-                <img src="public/images/han.jpg" alt="Dat" width="600" height="400">     
-            </a>
-            <div class="about__desc">
-                <p>
-                    Nguyễn Ngọc Hân
-                </p>
-                <ul>
-                    <li>MSSV</li>
-                    <li>SDT</li>
-                    <li>E-mail</li>
-                </ul>
-            </div>
+                    <img src="public/images/han.jpg" alt="Dat" width="600" height="400">
+                </a>
+                <div class="about__desc">
+                    <p>
+                        Nguyễn Ngọc Hân
+                    </p>
+                    <ul>
+                        <li>MSSV</li>
+                        <li>SDT</li>
+                        <li>E-mail</li>
+                    </ul>
+                </div>
             </div>
             <div class="about__gallery">
                 <a target="_blank" href="public/images/dung.jpg">
-                <img src="public/images/dung.jpg" alt="Dat" width="600" height="400">     
-            </a>
-            <div class="about__desc">
-                <p>
-                  Phan Hoàng Dung 
-                </p>
-                <ul>
-                    <li>MSSV</li>
-                    <li>SDT</li>
-                    <li>Sở thích</li>
-                </ul>
+                    <img src="public/images/dung.jpg" alt="Dat" width="600" height="400">
+                </a>
+                <div class="about__desc">
+                    <p>
+                        Phan Hoàng Dung
+                    </p>
+                    <ul>
+                        <li>MSSV</li>
+                        <li>SDT</li>
+                        <li>Email</li>
+                    </ul>
+                </div>
             </div>
-            </div>
-            </div> 
-            </div>
+        </div>
+    </div>
+</div>
 <?php
 include "template/footer.php";
