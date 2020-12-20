061c80e7f6348ecfbc9289a249bb5a7066b969fc
18520566 <73072113+18520566@users.noreply.github.com>
2020-12-20 21:45:40 +0700

    loginFirst - permissiondenied - update 1

--- application/views/permission-denied.php
+++ application/views/permission-denied.php
@@ -1,1 +1,20 @@
-per
+<?php
+include "template/header.php";
+include "template/nav.php"; ?>
+<div class="container">
+    <div class=".error">
+        <div class="error__body">
+            <p class="error__title">PERMISSION DENIED</p>
+            <p class="error__detail">You are not an administrator!</p>
+            <p class="error__more-detail">
+                The page you're looking for is denied. <br>
+                Try again or use go back button below.
+            </p>
+            <p>
+                <a href="home" class="error__button error__button--large">
+                    &LeftArrow; Go back
+                </a>
+            </p>
+        </div>
+    </div>
+</div>
