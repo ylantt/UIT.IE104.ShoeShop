a59edc1a962918cae6917ce0db94d30a5de1823f
Lan <truongylan2000@gmail.com>
2020-12-21 08:29:28 +0700

    home page update

--- application/views/home.php
+++ application/views/home.php
@@ -31,9 +31,9 @@ require_once("template/banner.php");
      <div class="brand">
           <div class="container">
                <div class="brand__list d-flex">
-                    <div class="brand--column brand--column-circle"><a href="#"><img src="./public/images/palladium-logo.jpg" alt="Palladium logo"></a></div>
-                    <div class="brand--column brand--column-rec-1"><a href="#"><img src="./public/images/converse-logo.png" alt="Converse logo"></a></div>
-                    <div class="brand--column brand--column-rec-2"><a href="#"><img src="./public/images/vans-logo.png" alt="Vans logo"></a></div>
+                    <div class="brand--column brand--column-circle"><a href="/palladium"><img src="./public/images/palladium-logo.jpg" alt="Palladium logo"></a></div>
+                    <div class="brand--column brand--column-rec-1"><a href="/converse"><img src="./public/images/converse-logo.png" alt="Converse logo"></a></div>
+                    <div class="brand--column brand--column-rec-2"><a href="/vans"><img src="./public/images/vans-logo.png" alt="Vans logo"></a></div>
                </div>
           </div>
      </div>
@@ -75,105 +75,104 @@ require_once("template/banner.php");
                               </div>
                          <?php endforeach; ?>
                     </div>
-                    <a href="products" class="section__link">Show All <span>&#8594;</span></a>
                </div>
           </div>
-
-          <div class="event">
-               <div class="container d-flex">
-                    <div class="event__info">
-                         <p class="event__title">HOT DEAL THIS YEAR</p>
-                         <p class="event__content">GET AN EXTRA <span class="text-bold">30% OFF</span> YOUR FIRST ORDER</p>
-                         <div class="event__time text-bold">
-                              <div class="event__time--column">
-                                   <p class="event__time--num event__time--day">423</p>
-                                   <p class="event__time--text">DAY(S)</p>
-                              </div>
-
-                              <div class="event__time--column">
-                                   <p class="event__time--num event__time--hour">23</p>
-                                   <p class="event__time--text">HOUR(S)</p>
-                              </div>
-
-                              <div class="event__time--column">
-                                   <p class="event__time--num event__time--min">23</p>
-                                   <p class="event__time--text">MIN</p>
-                              </div>
-
-                              <div class="event__time--column">
-                                   <p class="event__time--num event__time--sec">23</p>
-                                   <p class="event__time--text">SEC</p>
-                              </div>
-                         </div>
-                    </div>
-                    <div class="event__image"></div>
-               </div>
-          </div>
-
-          <div class="container">
-               <div class="service">
-                    <div class="service__item">
-                         <div class="service__icon">
-                              <i class="fa fa-shipping-fast"></i>
-                         </div>
-
-                         <div class="service__info">
-                              <h4 class="service__heading heading-h4">
-                                   FREE SHIPPING
-                              </h4>
-                              <p class="service__content">
-                                   Free shipping on all Vietnam order
-                              </p>
-                         </div>
-                    </div>
-
-                    <div class="service__item">
-                         <div class="service__icon">
-                              <i class="fas fa-phone-volume"></i>
-                         </div>
-
-                         <div class="service__info">
-                              <h4 class="service__heading heading-h4">
-                                   ONLINE SUPPORT 24/7
-                              </h4>
-                              <p class="service__content">
-                                   Support online 24 hours a day
-                              </p>
-                         </div>
-                    </div>
-
-                    <div class="service__item">
-                         <div class="service__icon">
-                              <i class="far fa-money-bill-alt"></i>
-                         </div>
-
-                         <div class="service__info">
-                              <h4 class="service__heading heading-h4">
-                                   MONEY RETURN
-                              </h4>
-                              <p class="service__content">
-                                   Back guarantee under 7 days
-                              </p>
-                         </div>
-                    </div>
-
-                    <div class="service__item">
-                         <div class="service__icon">
-                              <i class="far fa-id-card"></i>
-                         </div>
-
-                         <div class="service__info">
-                              <h4 class="service__heading heading-h4">
-                                   MEMBER DISCOUNT
-                              </h4>
-                              <p class="service__content">
-                                   On every order over $120.00
-                              </p>
-                         </div>
-                    </div>
-               </div>
-          </div>
-
      </div>
-     <?php
-     require_once("template/footer.php");
+
+     <div class="event">
+          <div class="container d-flex">
+               <div class="event__info">
+                    <p class="event__title">HOT DEAL THIS YEAR</p>
+                    <p class="event__content">GET AN EXTRA <span class="text-bold">30% OFF</span> YOUR FIRST ORDER</p>
+                    <div class="event__time text-bold">
+                         <div class="event__time--column">
+                              <p class="event__time--num event__time--day">423</p>
+                              <p class="event__time--text">DAY(S)</p>
+                         </div>
+
+                         <div class="event__time--column">
+                              <p class="event__time--num event__time--hour">23</p>
+                              <p class="event__time--text">HOUR(S)</p>
+                         </div>
+
+                         <div class="event__time--column">
+                              <p class="event__time--num event__time--min">23</p>
+                              <p class="event__time--text">MIN</p>
+                         </div>
+
+                         <div class="event__time--column">
+                              <p class="event__time--num event__time--sec">23</p>
+                              <p class="event__time--text">SEC</p>
+                         </div>
+                    </div>
+               </div>
+               <div class="event__image"></div>
+          </div>
+     </div>
+
+     <div class="service">
+          <div class="container">
+               <div class="service__item">
+                    <div class="service__icon">
+                         <i class="fa fa-shipping-fast"></i>
+                    </div>
+
+                    <div class="service__info">
+                         <p class="service__heading">
+                              FREE SHIPPING
+                         </p>
+                         <p class="service__content">
+                              Free shipping on all Vietnam order
+                         </p>
+                    </div>
+               </div>
+
+               <div class="service__item">
+                    <div class="service__icon">
+                         <i class="fas fa-phone-volume"></i>
+                    </div>
+
+                    <div class="service__info">
+                         <p class="service__heading">
+                              ONLINE SUPPORT 24/7
+                         </p>
+                         <p class="service__content">
+                              Support online 24 hours a day
+                         </p>
+                    </div>
+               </div>
+
+               <div class="service__item">
+                    <div class="service__icon">
+                         <i class="far fa-money-bill-alt"></i>
+                    </div>
+
+                    <div class="service__info">
+                         <p class="service__heading">
+                              MONEY RETURN
+                         </p>
+                         <p class="service__content">
+                              Back guarantee under 7 days
+                         </p>
+                    </div>
+               </div>
+
+               <div class="service__item">
+                    <div class="service__icon">
+                         <i class="far fa-id-card"></i>
+                    </div>
+
+                    <div class="service__info">
+                         <p class="service__heading">
+                              MEMBER DISCOUNT
+                         </p>
+                         <p class="service__content">
+                              On every order over $120.00
+                         </p>
+                    </div>
+               </div>
+          </div>
+     </div>
+</div>
+<?php
+require_once("template/footer.php");
